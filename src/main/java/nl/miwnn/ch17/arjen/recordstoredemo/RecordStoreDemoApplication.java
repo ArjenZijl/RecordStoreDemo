0bced9e4d33234e116d2c128b1580342458f3149
Clampiee <zijlstaarjen@hotmail.com>
2025-10-28 14:26:36 +0100

    Initial commit

---
+++ src/main/java/nl/miwnn/ch17/arjen/recordstoredemo/RecordStoreDemoApplication.java
@@ -0,0 +1,13 @@
+package nl.miwnn.ch17.arjen.recordstoredemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordStoreDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecordStoreDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/miwnn/ch17/arjen/recordstoredemo/RecordStoreDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.miwnn.ch17.arjen.recordstoredemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecordStoreDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
